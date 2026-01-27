1184dfa1538bd9740fa4ff1a9fb6f007d2b43ea2
juan-camposc <a_jcamposc@iesjabalcuz.es>
2026-01-27 18:06:34 +0100

    Add Home component for POKEPARTY landing page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>¡Bienvenido a POKEPARTY!</h1>
+      <p>¡Juega a los mejores minijuegos de Pokémon con tus amigos!</p>
+    </div>
+  );
+}
