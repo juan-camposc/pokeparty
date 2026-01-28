3e758bde0ea5a6c48219129fc7eb2e3a48042b16
juan-camposc <a_jcamposc@iesjabalcuz.es>
2026-01-28 16:51:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "raw.githubusercontent.com", // PokéAPI imágenes oficiales
+      "assets.pokemon.com"          // Otra fuente de imágenes de Pokémon
+    ],
+  },
+  env: {
+    // Puedes agregar variables de entorno si las necesitas
+    // EJ: API_KEY: process.env.API_KEY
+  },
+};
+
+module.exports = nextConfig;
 
